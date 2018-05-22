21db48652426f79287def2f53b53b76d9f6deda5
tangjie <alex_tang_151@163.com>
2018-05-22 13:37:04 +0800

    branch 分支修改
    branch 分支修改

--- SwiftDemo/SwiftDemo/ViewController.swift
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let lab = UILabel.init(frame: CGRect.init(x: 50, y: 100, width: 250, height: 100))
-    
+    let lab1 = UILabel.init(frame: CGRect.init(x: 50, y: 220, width: 250, height: 100))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         lab.textColor = UIColor.red
         lab.textAlignment = NSTextAlignment.center
         self.view.addSubview(lab)
+        
+        lab1.backgroundColor = UIColor.green
+        lab1.text = "branch 分支"
+        lab1.textColor = UIColor.red
+        lab1.textAlignment = NSTextAlignment.center
+        self.view.addSubview(lab1)
     }
 
     override func didReceiveMemoryWarning() {
