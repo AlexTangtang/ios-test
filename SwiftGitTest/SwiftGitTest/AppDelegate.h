b4735fa76f08546145f4c88a6fb06ad4a733c957
tangjie <alex_tang_151@163.com>
2018-05-22 09:32:53 +0800

    add a project
    add a project

---
+++ SwiftGitTest/SwiftGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftGitTest
+//
+//  Created by 唐桔 on 2018/5/22.
+//  Copyright © 2018年 tangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
