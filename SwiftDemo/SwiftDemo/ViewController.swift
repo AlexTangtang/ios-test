71b8cfd536b56f4fe154f3b78e1b479b030c8025
tangjie <alex_tang_151@163.com>
2018-05-22 13:30:48 +0800

    add a new project
    add a new project

---
+++ SwiftDemo/SwiftDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SwiftDemo
+//
+//  Created by 唐桔 on 2018/5/22.
+//  Copyright © 2018年 tangjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let lab = UILabel.init(frame: CGRect.init(x: 50, y: 100, width: 250, height: 100))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        lab.backgroundColor = UIColor.green
+        lab.text = "master 主分支"
+        lab.textColor = UIColor.red
+        lab.textAlignment = NSTextAlignment.center
+        self.view.addSubview(lab)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
